df3cbfcce94246f9847a16fbb28de5589936c75e
Ethem Dalmis <82269054+edalmis@users.noreply.github.com>
2021-05-10 14:26:39 +0200

    Update get_next_line_bonus.c
    Library changed*

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
 #include "get_next_line_bonus.h"
 
 size_t	ft_strlen(char *s)
